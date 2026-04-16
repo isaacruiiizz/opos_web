338f85d3ca9facd35b199014d71c770cb4c36f83
isaacruiiiz <isaky032005@gmail.com>
2026-04-16 21:20:34 +0200

    feat: add ReadingProgressBar component

---
+++ frontend/src/components/apunts/ReadingProgressBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h-[3px] bg-[var(--color-border)] sticky z-[9]" :style="{ top: topOffset + 'px' }">
+    <div class="h-full bg-gradient-to-r from-primary to-blue-400 transition-[width] duration-200"
+         :style="{ width: pct + '%' }"></div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  pct: { type: Number, default: 0 },
+  topOffset: { type: Number, default: 45 },
+})
+</script>
